548155315c5b4d7907fe34a36a1f6c3845656736
Yvan Satyawan <y_satyawan@hotmail.com>
2020-01-30 14:01:38 +0100

    Way more checks to make sure prev process has been killed as well as making sure that the port is actually available

--- hpoptim/train_sweep_qualitai.py
+++ hpoptim/train_sweep_qualitai.py
@@ -23,9 +23,12 @@ Created on:
 """
 import argparse
 import os
+from signal import SIGKILL, SIGTERM
 import subprocess
 import sys
 from datetime import datetime
+import socket
+from time import sleep
 
 import torch
 
@@ -88,18 +91,87 @@ def create_run_config(base_config, name,
     return out_path
 
 
+def test_prev_pids():
+    """Makes sure previous process are dead.
+
+    Make sure prev processes have been terminated. Otherwise, wait up to 5
+    minutes. If processes still haven't died, then kill them.
+    """
+    retries = 0
+    prev_processes = []
+    # First get all prev processes
+    try:
+        with open('/tmp/mmdet_pids.txt', 'r') as pid_file:
+            for line in pid_file:
+                prev_processes.append(line.strip())
+    except FileNotFoundError:
+        pass
+
+    while retries <= 60 and prev_processes:
+        retries += 1
+        proc_list = (subprocess.Popen(["ps", "-o", "pid"],
+                                      stdout=subprocess.PIPE)
+                     .communicate())[0].decode('utf-8').splitlines()[1:]
+        for pid in prev_processes:
+            # Check to see if there are any processes still running. If they
+            # have already been killed of have died, remove them from the list.
+            if pid not in proc_list:
+                prev_processes.remove(pid)
+
+        if prev_processes:
+            for pid in prev_processes:
+                os.kill(int(pid), SIGTERM)
+            sleep(5.)
+
+    # Final sigkill if there are still processes
+    if prev_processes:
+        for pid in prev_processes:
+            os.kill(int(pid), SIGKILL)
+
+
+def get_available_port(addr):
+    """Tries to get the next available port.
+
+    Args:
+        addr (str): Address to test ports on.
+
+    Raises:
+        ConnectionError: If no ports are available a ConnectionError will be
+            raised.
+    Returns:
+        int: The next available port.
+    """
+    port = 32768
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        while s.connect_ex((addr, port)) == 0 and port < 61001:
+            port += 1
+
+    if port >= 61001:
+        raise ConnectionError('No ports are available for torch.distributed')
+
+    return port
+
+
 def main(arguments):
 
     # world size in terms of number of processes/GPUs
     dist_world_size = torch.cuda.device_count()
 
+    test_prev_pids()
+
+    # Now final check to make sure that address and port is valid and available.
+    addr = '127.0.0.1'
+    port = get_available_port(addr)
+
     # set PyTorch distributed related environmental variables
     current_env = os.environ.copy()
-    current_env["MASTER_ADDR"] = '127.0.0.1'
-    current_env["MASTER_PORT"] = str(29500)
+    current_env["MASTER_ADDR"] = addr
+    current_env["MASTER_PORT"] = str(port)
     current_env["WORLD_SIZE"] = str(dist_world_size)
 
     processes = []
+    process_pids = []
 
     if 'OMP_NUM_THREADS' not in os.environ:
         current_env["OMP_NUM_THREADS"] = str(12)
@@ -108,8 +180,8 @@ def main(arguments):
               "to be {} in default, to avoid your system being overloaded, "
               "please further tune the variable for optimal performance in "
               "your application as needed. \n"
-              "*****************************************".format(
-            current_env["OMP_NUM_THREADS"]))
+              "*****************************************"
+              .format(current_env["OMP_NUM_THREADS"]))
 
     for local_rank in range(0, dist_world_size):
         # each process's rank
@@ -118,11 +190,12 @@ def main(arguments):
         current_env["LOCAL_RANK"] = str(local_rank)
 
         # spawn the processes
-        cmd = [sys.executable, "-u"]
-
-        cmd.append("/workspace/mmdetection/tools/train.py")
-
-        cmd.append("--local_rank={}".format(local_rank))
+        cmd = [
+            sys.executable,
+            "-u",
+            "/workspace/mmdetection/tools/train.py",
+            "--local_rank={}".format(local_rank)
+        ]
 
         config_name = "sweep--"\
                       + datetime.now().strftime("%Y_%m_%d--%H_%M_%S.py")
@@ -135,7 +208,13 @@ def main(arguments):
         cmd.extend([config_path, "--validate", '--launcher', 'pytorch'])
 
         process = subprocess.Popen(cmd, env=current_env)
+
         processes.append(process)
+        process_pids.append('{}\n'.format(process.pid))
+
+    # Write process PIDs so we can check for them next run.
+    with open('/tmp/mmdet_pids.txt', 'w') as pid_file:
+        pid_file.writelines(process_pids)
 
     for process in processes:
         process.wait()
