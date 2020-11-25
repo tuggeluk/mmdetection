e54606fd3a64d1eaecd66a9e42348eb7166c6ffd
Lukas Tuggener <tugg@zhaw.ch>
2020-11-25 17:37:04 +0100

    update individual configs

--- configs/DeepScoresBaselines/DSv2/dsv2_wfcos_hrnet_normal.py
+++ configs/DeepScoresBaselines/DSv2/dsv2_wfcos_hrnet_normal.py
@@ -1,5 +1,4 @@
 _base_ = [
-    '../../_base_/datasets/dsV2_detection.py',
     '../../_base_/schedules/schedule_1x.py', '../../_base_/default_runtime.py'
 ]
 # model settings
@@ -111,22 +110,64 @@ log_config = dict(
         dict(type='WandbVisualLoggerHook')
     ])
 # specific dataset configs
+dataset_type = 'DeepScoresV2Dataset'
+data_root = 'data/ds2_dense/'
+
 img_norm_cfg = dict(
     mean=[240, 240, 240],
     std=[57, 57, 57],
     to_rgb=False)
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=tuple((2700, 3828)), keep_ratio=True),
     #dict(type='Resize', img_scale=tuple((1400, 1920)), keep_ratio=True),
-    dict(type='RandomCrop', crop_size=(1000, 1000)),
+    dict(type='RandomCrop', crop_size=(1000, 500)),
     dict(type='RandomFlip', flip_ratio=0.0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(2700, 3828),
+        flip=False,
+        transforms=[
+            dict(type='Resize', img_scale=tuple((2700, 3828)), keep_ratio=True),
+            #dict(type='Resize', img_scale=(1400, 1920), keep_ratio=True),
+            #dict(type='RandomCrop', crop_size=(800, 800)),
+            dict(type='RandomFlip', flip_ratio=0.0),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    imgs_per_gpu=1,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'deepscores_train.json',
+        img_prefix=data_root + 'images/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'deepscores_test.json',
+        img_prefix=data_root + 'images/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'deepscores_test.json',
+        img_prefix=data_root + 'images/',
+        pipeline=test_pipeline))
+
+evaluation = dict(interval=80, metric='bbox')
 
 # yapf:enable
 # runtime settings
