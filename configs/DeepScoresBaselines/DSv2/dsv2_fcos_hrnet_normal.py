cc9743694c50d8b0510a5cc9a5c56cd0490c1abc
Lukas Tuggener <tugg@zhaw.ch>
2020-11-26 09:46:52 +0100

    update individual configs

--- configs/DeepScoresBaselines/DSv2/dsv2_fcos_hrnet_normal.py
+++ configs/DeepScoresBaselines/DSv2/dsv2_fcos_hrnet_normal.py
@@ -76,7 +76,7 @@ test_cfg = dict(
 # optimizer
 optimizer = dict(
     type='SGD',
-    lr=0.01,
+    lr=0.001,
     momentum=0.9,
     weight_decay=0.0001,
     paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
